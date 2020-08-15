dba53d9aacc08ef35f2ed93701f1c585a1389d2d
iftekhar <iftysoftwares02@gmail.com>
2020-08-15 22:19:24 +0600

    T2M2F2

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,9 +9,78 @@ router.get('/', function(req, res){
         res.render('admin/index');
     }
     else{
-        res.redirect('/login');
+        res.redirect('/employee');
+    }	
+});
+
+router.get('/addemployee', function(req, res){
+    
+    if(req.cookies['type'] == 'admin'){
+        
+        var value = {
+            name: req.body.name,
+            username: req.body.username,
+            phone: req.body.phone,
+            gender: req.body.gender,
+            designation: req.body.designation,
+        };
+        var valueCheck = [];
+        var data ={
+            value   : value,
+            errors  : valueCheck
+        }
+        res.render('admin/addEmployee',data);
+    }
+    else{
+        res.redirect('/employee');
     }
     
-	
 });
+router.post('/addemployee', [
+    body('name','Name required!').notEmpty(),
+    body('username').notEmpty().withMessage('Username required!').isLength({min: 9}).withMessage('Username must be greater than 8 characters!'),
+    body('password').notEmpty().withMessage('Password required!').isLength({min: 8}).withMessage('Password must be atleast 8 characters!').custom((value,{req}) => {
+        if (value != req.body.cPassword) {
+            throw new Error("Passwords don't match");
+        } else {
+            return value;
+        }
+    }),
+    body('phone').notEmpty().withMessage('Phone number required!').isNumeric().withMessage('Phone number must be numeric!').isLength({min: 11, max: 11}).withMessage('Phone number must be 11 characters!'),
+    body('gender','Gender required!').notEmpty(),
+], function(req, res){
+    if(req.cookies['type'] == 'admin'){
+        
+        var value = {
+            name: req.body.name,
+            phone: req.body.phone,
+            username: req.body.username,
+            gender: req.body.gender,
+            designation: req.body.designation,
+        };
+        
+        var valueCheck = [];
+
+        validationResult(req).errors.forEach(error => {
+            valueCheck.push(error.msg);
+        });
+        var data ={
+            value   : value,
+            errors  : valueCheck
+        }
+        if(valueCheck.length > 0){
+            res.render('admin/addemployee',data);
+        } else {
+            admindb.addEmployee(value, function(result){
+                console.log(result);
+
+            });
+        }
+    }
+    else{
+        res.redirect('/employee');
+    }
+
+});
+
 module.exports = router;
--- models/admin.js
+++ models/admin.js
@@ -7,10 +7,22 @@ module.exports = {
         db.getResults(sql,function(result){
             if(result.length>0){
                 callback(result);
+                
             }
             else{
                 callback([]);
             }
         });
-    }   
+    },
+
+    addEmployee: function(value, callback){
+        var sql="INSERT INTO `login_cred` (`username`, `password`, `type`) VALUES ('"+value.username+"','"+value.password+"','employee');";
+        db.getResults(sql,function(result){
+            var sql1="INSERT INTO `personal_details`(`u_id`, `name`, `phone`, `gender`, `designation`) VALUES ('"+result.insertId+"','"+value.name+"','"+value.phone+"','"+value.gender+"','"+value.designation+"');";
+            db.getResults(sql1,function(result1){
+                callback(result1);
+            });
+        });
+    },
+
 }
